b789facf208287d99444f18a6f5f20597a10a8b6
Artus U <artusrock@hotmail.com>
2022-12-14 09:50:49 +0000

    frontpage

--- ttt/urls.py
+++ ttt/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.core.views import frontpage
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", frontpage, name="frontpage"),
 ]
