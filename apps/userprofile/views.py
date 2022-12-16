94d7b6c95eb3bc45a8e09410aef58b5eec0748b5
Artus U <artusrock@hotmail.com>
2022-12-16 14:21:09 +0000

    TTT10-4 Create view and template for sign up Fixes #2

--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from apps.userprofile.models import Userprofile
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            user.email = user.username
+            user.save()
+
+            userprofile = Userprofile.objects.create(user=user)
+            login(request, user)
+            return redirect('frontpage')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'userprofile/signup.html', {'form': form})
