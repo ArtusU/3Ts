36e2eb68d12c7c91ba22d50ca829df645a1d693f
Artus U <artusrock@hotmail.com>
2022-12-21 12:19:38 +0000

    userprofile urls clean-up

--- ttt/urls.py
+++ ttt/urls.py
@@ -1,24 +1,9 @@
-"""ttt URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 
 from apps.core.views import frontpage, privacy, terms, plans
-from apps.userprofile.views import signup, myaccount, edit_profile
+from apps.userprofile.views import signup
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -31,6 +16,5 @@ urlpatterns = [
     path("signup/", signup, name="signup"),
     path("login/", auth_views.LoginView.as_view(template_name="userprofile/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("myaccount/", myaccount, name="myaccount"),
-    path("myaccount/edit_profile/", edit_profile, name="edit_profile"),
+    path("myaccount/", include('apps.userprofile.urls')),
 ]
