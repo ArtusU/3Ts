2946b73e3dbe808a36cde0e6c98636365e82454d
Artus U <artusrock@hotmail.com>
2023-01-10 10:12:11 +0000

    user projects list

--- apps/project/views.py
+++ apps/project/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from .models import Project
+from apps.team.models import Team
+
+
+@login_required
+def projects(request):
+    team = get_object_or_404(
+        Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE
+    )
+    projects = team.projects.all()
+
+    if request.method == "POST":
+        title = request.POST.get("title")
+
+        if title:
+            Project.objects.create(team=team, title=title, created_by=request.user)
+            messages.info(request, "The project was added!")
+            return redirect("project:projects")
+
+    return render(
+        request, "project/projects.html", {"team": team, "projects": projects}
+    )
