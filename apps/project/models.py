cc44926ed2ad5aa0f1f9a6f14d5591bd8dc4b186
Artus U <artusrock@hotmail.com>
2023-01-10 08:48:28 +0000

    project app and basic model

---
+++ apps/project/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from apps.team.models import Team
+
+
+class Project(models.Model):
+    team = models.ForeignKey(Team, related_name='projects', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+
+    created_by = models.ForeignKey(User, related_name='projects', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['title']
+    
+    def __str__(self):
+        return self.title
+    
+    def registered_time(self):
+        return 0
+    
+    def num_tasks_todo(self):
+        return 0
