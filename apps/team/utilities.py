be5dff1b3ac36c7ecac4c27d89f0d70e706fd712
Artus U <artusrock@hotmail.com>
2023-02-17 11:35:29 +0000

    Accepting an invite view and template Fixes #48

--- apps/team/utilities.py
+++ apps/team/utilities.py
@@ -7,20 +7,28 @@ def send_invitation(to_email, code, team):
     from_email = settings.DEFAULT_EMAIL_FROM
     acceptation_url = settings.ACCEPTATION_URL
 
-    subject = 'Invitation to 3Ts'
-    text_content = 'Invitation to Teams Time Tracker - 3Ts. Your code is: %s' % code
-    html_content = render_to_string('team/email_invitation.html', {'code': code, 'team': team, 'acceptation_url': acceptation_url})
+    subject = "Invitation to 3Ts"
+    text_content = "Invitation to Teams Time Tracker - 3Ts. Your code is: %s" % code
+    html_content = render_to_string(
+        "team/email_invitation.html",
+        {"code": code, "team": team, "acceptation_url": acceptation_url},
+    )
 
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
-    msg.attach_alternative(html_content, 'text/html')
+    msg.attach_alternative(html_content, "text/html")
     msg.send()
+
 
 def send_invitation_accepted(team, invitation):
     from_email = settings.DEFAULT_EMAIL_FROM
-    subject = 'Invitation accepted'
-    text_content = 'Your invitation was accepted'
-    html_content = render_to_string('team/email_accepted_invitation.html', {'team': team, 'invitation': invitation})
+    subject = "Invitation accepted"
+    text_content = "Your invitation was accepted"
+    html_content = render_to_string(
+        "team/email_accepted_invitation.html", {"team": team, "invitation": invitation}
+    )
 
-    msg = EmailMultiAlternatives(subject, text_content, from_email, [team.created_by.email])
-    msg.attach_alternative(html_content, 'text/html')
+    msg = EmailMultiAlternatives(
+        subject, text_content, from_email, [team.created_by.email]
+    )
+    msg.attach_alternative(html_content, "text/html")
     msg.send()
