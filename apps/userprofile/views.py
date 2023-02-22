66e3df7a5a88420e6657c004d44fe9a10a269e64
Artus U <artusrock@hotmail.com>
2023-02-22 10:28:06 +0000

    Add users avatar Fixes #59

--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -10,28 +10,34 @@ from apps.team.utilities import send_invitation_accepted
 @login_required
 def myaccount(request):
     teams = request.user.teams.exclude(pk=request.user.userprofile.active_team_id)
-    return render(request, 'userprofile/myaccount.html', {'teams': teams})
+    return render(request, "userprofile/myaccount.html", {"teams": teams})
 
 
 @login_required
 def edit_profile(request):
-    if request.method == 'POST':
-        request.user.first_name = request.POST.get('first_name', '')
-        request.user.last_name = request.POST.get('last_name', '')
-        request.user.email = request.POST.get('email', '')
+    if request.method == "POST":
+        request.user.first_name = request.POST.get("first_name", "")
+        request.user.last_name = request.POST.get("last_name", "")
+        request.user.email = request.POST.get("email", "")
         request.user.save()
-        
-        messages.info(request, 'The changes was saved')
-        
-        return redirect('myaccount')
-    
-    return render(request, 'userprofile/edit_profile.html')
+
+        if request.FILES:
+            avatar = request.FILES["avatar"]
+            userprofile = request.user.userprofile
+            userprofile.avatar = avatar
+            userprofile.save()
+
+        messages.info(request, "The changes was saved")
+
+        return redirect("myaccount")
+
+    return render(request, "userprofile/edit_profile.html")
 
 
 @login_required
 def accept_invitation(request):
-    if request.method == 'POST':
-        code = request.POST.get('code')
+    if request.method == "POST":
+        code = request.POST.get("code")
 
         invitations = Invitation.objects.filter(code=code, email=request.user.email)
 
@@ -48,13 +54,12 @@ def accept_invitation(request):
             userprofile.active_team_id = team.id
             userprofile.save()
 
-            messages.info(request, 'Invitation accepted')
+            messages.info(request, "Invitation accepted")
 
             send_invitation_accepted(team, invitation)
 
-            return redirect('team:team', team_id=team.id)
+            return redirect("team:team", team_id=team.id)
         else:
-            messages.info(request, 'Invitation was not found')
+            messages.info(request, "Invitation was not found")
     else:
-        return render(request, 'userprofile/accept_invitation.html')
-
+        return render(request, "userprofile/accept_invitation.html")
