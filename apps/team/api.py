8ad44e03739467be8bd1f6451d3c0cf927860b9e
Artus U <artusrock@hotmail.com>
2023-03-30 10:13:06 +0100

    stripe_webhook

--- apps/team/api.py
+++ apps/team/api.py
@@ -37,3 +37,33 @@ def create_checkout_session(request):
         return JsonResponse({'sessionId': checkout_session['id']})
     except Exception as e:
         return JsonResponse({'error': str(e)})
+
+@csrf_exempt
+def stripe_webhook(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    webhook_key = settings.STRIPE_WEBHOOK_KEY
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, webhook_key
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+    
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        team = Team.objects.get(pk=session.get('client_reference_id'))
+        team.stripe_customer_id = session.get('customer')
+        team.stripe_subscription_id = session.get('subscription')
+        team.save()
+
+        print('Team %s subscribed to a plan' % team.title)
+    
+    return HttpResponse(status=200)
